2d832cfe54ed78a2ad9aec91f12487baef62a2ab
Trường Huỳnh <huynhnhattruonglt@gmail.com>
2023-03-20 22:42:02 +0700

    add comment

--- pl-react/src/components/HowToPlayDialog.js
+++ pl-react/src/components/HowToPlayDialog.js
@@ -5,7 +5,7 @@ import DialogContent from "@mui/material/DialogContent";
 import DialogContentText from "@mui/material/DialogContentText";
 import DialogTitle from "@mui/material/DialogTitle";
 import Button from "@mui/material/Button";
-
+// how
 const HowToPlayDialog = ({ openState, onClose, dRef }) => (
   <Dialog
     open={openState}
